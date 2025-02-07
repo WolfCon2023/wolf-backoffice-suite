c47e7120b74adfe05b8b32aefa9c4a5b6b2ca927
WolfCon2023 <ccarter@wolfconsultingnc.com>
2025-02-07 16:52:06 -0500

    Import all local files

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Backend API is running!'));
+
+app.listen(process.env.PORT, () => console.log('Server running on port', process.env.PORT));
