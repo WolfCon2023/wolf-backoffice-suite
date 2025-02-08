d78b394ab505752ba023709847b0eacbcc44709b
WolfCon2023 <ccarter@wolfconsultingnc.com>
2025-02-08 11:54:49 -0500

    Update server.js

--- wolf-backoffice-apps/server.js
+++ wolf-backoffice-apps/server.js
@@ -64,6 +64,15 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use("/api/auth", authRoutes);
 app.use("/api/customers", customerRoutes);
 
+process.on("uncaughtException", (err) => {
+  console.error("❌ Uncaught Exception:", err);
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("❌ Unhandled Rejection:", reason);
+});
+
+
 // Start Server
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
