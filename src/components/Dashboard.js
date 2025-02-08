b7178c90cbb55fd439f7e02ba5b622621d7f8b89
WolfCon2023 <ccarter@wolfconsultingnc.com>
2025-02-07 19:13:37 -0500

    Create Dashboard.js

---
+++ src/components/Dashboard.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Vital Back Office Applications Menu</h1>
+      <nav>
+        <Link to="/appointments">Appointments</Link>
+        <Link to="/calendar">Calendar</Link>
+        <Link to="/customers">Customers</Link>
+        <Link to="/crm">CRM</Link>
+      </nav>
+    </div>
+  );
+};
+
+export default Dashboard;
